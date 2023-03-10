4221500d7caba83d0c3ff7ed32e800874235016a
teslabyte <15111613+teslabyte@users.noreply.github.com>
2023-03-10 12:59:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Enter 216 character length identifier
+
+using DokkanIdentifierConvertor;
+
+Console.WriteLine("Enter identifier :");
+string ident_216 = Console.ReadLine();
+
+Security security = new Security();
+
+string ident_152 = security.DecryptIdentifier(ident_216);
+
+Console.WriteLine("Converted identifier:\n" + ident_152);
+
+
---
+++ Security.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DokkanIdentifierConvertor
+{
+    internal class Security
+    {
+        private string aesKey = ""; //can't share the key on github
+
+        public string DecryptIdentifier(string identifier)
+        {
+            return DecryptStringFromBytes(identifier,aesKey);
+        }
+
+        private string DecryptStringFromBytes(string cipherText, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = keyBytes;
+                aesAlg.Mode = CipherMode.ECB;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                byte[] decryptedBytes = decryptor.TransformFinalBlock(cipherTextBytes, 0, cipherTextBytes.Length);
+
+                string plaintext = Encoding.UTF8.GetString(decryptedBytes);
+
+                return plaintext;
+            }
+        }
+    }
+}
